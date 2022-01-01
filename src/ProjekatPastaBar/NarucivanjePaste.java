05fe6861bcea3ac133beba2f2a0211fb4aa4b65f
Stefan Stojiljkovic <stefanstojiljkovic.ni@gmail.com>
2022-01-01 18:57:38 +0100

    Dodata je WHILE petlja

--- src/ProjekatPastaBar/NarucivanjePaste.java
+++ src/ProjekatPastaBar/NarucivanjePaste.java
@@ -16,7 +16,57 @@ public class NarucivanjePaste {
 		int[] prices = { 50, 60, 120, 120, 140, 100, 100, 100, 80, 50, 80, 80, 80, 50, 50, 20 };
 		
 		String[] phoneNumbers = { "063111111", "063222222", "063333333", "064444444", "065555555", "066666666" };
+		
 
+		boolean stopOrder = false;
+		int orderPrice = 0;
+
+		System.out.println("Dobrodosli, izvolite");
+
+		while (stopOrder == false) {
+
+			while (stopOrder == false) {
+
+				System.out.print("Odaberite sastojak: ");
+				String ingridient = s.next();
+
+				if (!ingridient.equals("poruci") && findIngredient(ingridients, ingridient) != 0) {
+
+					orderPrice = orderPrice + prices[findIngredient(ingridients, ingridient)];
+
+					System.out.println("Uzeli ste " + ingridient + " ukupno " + orderPrice + "rsd");
+
+				} else if (ingridient.equals("poruci")) {
+
+					System.out.print("Unesite Vas broj telefona: ");
+					String phone = s.next();
+					System.out.println();
+
+					if (isRegularCustomer(phoneNumbers, phone) == true) {
+
+						orderPrice = orderPrice - (orderPrice * 10 / 100);
+
+						System.out.println("Redovne musterije imaju popust od 10 %");
+
+					}
+
+					stopOrder = true;
+					System.out.println("Cena paste je: " + orderPrice + "rsd");
+					System.out.println("Prijatno!");
+				
+				} else {
+					System.out.println();
+					System.out.println("Trenutno nemamo taj sastojak.");
+					System.out.println("Odaberite drugi sastojak.");
+
+					for (int i = 0; i < ingridients.length; i++) {
+						System.out.print(ingridients[i] + ", ");
+					}
+					System.out.println();
+					System.out.println();
+				}
+			}
+			
+		}
 	}
-
 }
